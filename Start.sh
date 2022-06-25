bcc7a77f1465ed0d65f11db3f9791e9f59a8ab81
Ivanich41 <77790965+Ivanich41@users.noreply.github.com>
2022-06-25 22:57:19 +0300

    Update Start.sh

--- Start.sh
+++ Start.sh
@@ -1,35 +1,6 @@
 #!/bin/bash
 NAME="example" # input .conf name
- # Checking block 
-if ! [ -e /etc/vpncheck ]
-then
-	if ! command -v wg-quick >/dev/null 2>&1
-	then 
-		echo "Couldn't find wireguard (and wg-quick module) installed. Script stopping"
-		echo "Press anything to close this window"
-		read $anything
-		exit
-	fi
-	if ! command -v curl >/dev/null 2>&1
-	then
-		echo "Couldn't find curl installed. Script stopping"
-		echo "Press anything to close this window"
-		read $anything
-		exit
-	fi
-	if ! [ -e /etc/wireguard/$NAME.conf ] 
-	then
-		echo "There is no config in /etc/wireguard or no config with specified name. Check the manual"
-		echo "Press anything to close this window"
-		read $anything
-		exit
-	fi
-touch /etc/vpncheck  
-echo "Checkup succses"
-else
-	echo "Checkup succses"
-fi
-# Running block
+
 wg-quick up $NAME
 echo "Checking ip:"
 curl ifconfig.me
